5e1ee69097a739b903b6ae6bad646b13c388599d
txoof <aaron.ciuffo@gmail.com>
2022-02-09 11:32:24 +0100

    bump version

--- gdrive_audit/constants.py
+++ gdrive_audit/constants.py
@@ -3,7 +3,7 @@ from pathlib import Path
 dir_path = os.path.dirname(os.path.realpath(__file__))
 
 
-VERSION = '0.0.1'
+VERSION = '0.1.0'
 APP_NAME = 'gdrive_audit'
 SCOPES = ['https://www.googleapis.com/auth/drive']
 CACHE = Path(f'~/.cache/{APP_NAME}/').expanduser().resolve()
@@ -14,3 +14,5 @@ SECRETS_PATHS = {'saved': CACHE/SECRETS_FILE,
                 'downloaded': Path(f'~/Downloads/{SECRETS_FILE}').expanduser().resolve(),}
 
 AUDIT_FILTER = "https://docs.google.com/spreadsheets/d/15U7uA7O9yAX61g_WPvdeGGuEninmJbnnyzA7wbHIaw0/copy"
+
+README_GETTING_STARTED = "https://github.com/txoof/gdrive_audit#getting-started"
